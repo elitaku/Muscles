76940c75c7ee346b7e3161278fe16fe8c0e75f2b
Noseilos <jonaseguiron@gmail.com>
2024-03-18 23:35:39 +0800

    place order COD

--- screens/Payment.jsx
+++ screens/Payment.jsx
@@ -4,19 +4,115 @@ import { colors, defaultStyle } from "../styles/styles";
 import Header from "../components/Header";
 import Heading from "../components/Heading";
 import { Button, RadioButton } from "react-native-paper";
+import { useDispatch, useSelector } from "react-redux";
+import { placeOrder } from "../redux/actions/otherActions";
+import { useMessageAndErrorOther } from "../utils/hooks";
+// import { useStripe } from "@stripe/stripe-react-native";
+import { Toast } from "react-native-toast-message/lib/src/Toast";
+import axios from "axios";
+import { server } from "../redux/store";
+import Loader from "../components/Loader";
 
 const Payment = ({ navigation, route }) => {
   const [paymentMethod, setPaymentMethod] = useState("COD");
+  const [loaderLoading, setLoaderLoading] = useState(false);
 
-  const isAuthenticated = true;
+  const dispatch = useDispatch();
+  // const stripe = useStripe();
+
+  const { isAuthenticated, user } = useSelector((state) => state.user);
+  const { cartItems } = useSelector((state) => state.cart);
 
   const redirectToLogin = () => {
     navigation.navigate("login");
   };
-  const codHandler = () => {};
-  const onlineHandler = () => {};
+  const codHandler = (paymentInfo) => {
+    const shippingInfo = {
+      address: user.address,
+      city: user.city,
+      country: user.country,
+      pinCode: user.pinCode,
+    };
 
-  return (
+    const itemsPrice = route.params.itemsPrice;
+    const shippingCharges = route.params.shippingCharges;
+    const taxPrice = route.params.tax;
+    const totalAmount = route.params.totalAmount;
+
+    dispatch(
+      placeOrder(
+        cartItems,
+        shippingInfo,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingCharges,
+        totalAmount,
+        paymentInfo
+      )
+    );
+  };
+  const onlineHandler = async () => {
+    try {
+      const {
+        data: { client_secret },
+      } = await axios.post(
+        `${server}/order/payment`,
+        {
+          totalAmount: route.params.totalAmount,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+
+      const init = await stripe.initPaymentSheet({
+        paymentIntentClientSecret: client_secret,
+        merchantDisplayName: "6PackEcom",
+      });
+
+      if (init.error)
+        return Toast.show({ type: "error", text1: init.error.message });
+
+      const presentSheet = await stripe.presentPaymentSheet();
+      setLoaderLoading(true);
+
+      if (presentSheet.error) {
+        setLoaderLoading(false);
+        return Toast.show({ type: "error", text1: presentSheet.error.message });
+      }
+
+      const { paymentIntent } = await stripe.retrievePaymentIntent(
+        client_secret
+      );
+
+      if (paymentIntent.status === "Succeeded") {
+        codHandler({ id: paymentIntent.id, status: paymentIntent.status });
+      }
+    } catch (error) {
+      return Toast.show({
+        type: "error",
+        text1: "Some Error",
+        text2: error,
+      });
+    }
+  };
+
+  const loading = useMessageAndErrorOther(
+    dispatch,
+    navigation,
+    "profile",
+    () => ({
+      type: "clearCart",
+    })
+  );
+
+  return loaderLoading ? (
+    <Loader />
+  ) : (
     <View style={defaultStyle}>
       <Header back={true} />
       <Heading
@@ -44,7 +140,7 @@ const Payment = ({ navigation, route }) => {
       </View>
 
       <TouchableOpacity
-        //disabled={loading}
+        disabled={loading}
         onPress={
           !isAuthenticated
             ? redirectToLogin
@@ -52,12 +148,10 @@ const Payment = ({ navigation, route }) => {
             ? () => codHandler()
             : onlineHandler
         }
-      ></TouchableOpacity>
-
-      <TouchableOpacity>
+      >
         <Button
-          //loading={loading}
-          //disabled={loading}
+          loading={loading}
+          disabled={loading}
           style={styles.btn}
           textColor={colors.color2}
           icon={
